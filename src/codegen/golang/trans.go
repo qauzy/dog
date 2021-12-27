2ca8c980798c16d881c1ac254ef255ecef1e9329
qauzy <nonecheese@gmail.com>
2021-12-27 17:10:21 +0800

    feat:蛇形转驼峰

--- src/codegen/golang/trans.go
+++ src/codegen/golang/trans.go
@@ -367,3 +367,34 @@ func SnakeString(s string) string {
 	//ToLower把大写字母统一转小写
 	return strings.ToLower(string(data[:]))
 }
+
+/**
+ * 蛇形转驼峰
+ * @description xx_yy to XxYx  xx_y_y to XxYY
+ * @date 2020/7/30
+ * @param s要转换的字符串
+ * @return string
+ **/
+func CamelString(s string) string {
+	data := make([]byte, 0, len(s))
+	j := false
+	k := false
+	num := len(s) - 1
+	for i := 0; i <= num; i++ {
+		d := s[i]
+		if k == false && d >= 'A' && d <= 'Z' {
+			k = true
+		}
+		if d >= 'a' && d <= 'z' && (j || k == false) {
+			d = d - 32
+			j = false
+			k = true
+		}
+		if k && d == '_' && num > i && s[i+1] >= 'a' && s[i+1] <= 'z' {
+			j = true
+			continue
+		}
+		data = append(data, d)
+	}
+	return string(data[:])
+}
