09157fb882045e8ac7ecb8dd38595ca8cf674ddc
qauzy <nonecheese@gmail.com>
2022-03-21 10:25:18 +0800

    feat:LICENSE

--- src/cfg/config.go
+++ src/cfg/config.go
@@ -4,8 +4,8 @@ var (
 	ConstructFieldFunc = false //构建Get,Set函数
 	AllStatic          = true  //所有变量和方法按静态类型处理
 	AppendContext      = false //添加*gin.Contex
-	FieldAccess        = true  //Get,Set函数转换为直接成员访问
-	MapListIdxAccess   = true  //索引形式访问Map和List
+	FieldAccess        = false //Get,Set函数转换为直接成员访问
+	MapListIdxAccess   = false //索引形式访问Map和List
 	DropResult         = false //去掉返回值
 	OneFold            = false //独立文件夹
 	ConstructNewFunc   = false //是否构建构造New函数
@@ -13,7 +13,7 @@ var (
 	Capitalize         = true  //类成员大写开头
 	StarClassTypeParam = true  //非原生类型函数参数带*
 	StarClassTypeDecl  = true  //非原生类型变量带*
-	ParseOnly          = true  //只解析，不翻译(获取全局信息)
+	ParseOnly          = false //只解析，不翻译(获取全局信息)
 	ProjectName        = "bitrade"
 	SourceBase         = "/opt/code/ZTuoExchange_framework"                                        //待转换源代码工程目录
 	SourcePath         = "/opt/code/ZTuoExchange_framework/core/src/main/java/cn/ztuo/bitrade/dao" //待转换源代码目录
